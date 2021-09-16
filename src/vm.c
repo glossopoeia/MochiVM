3e3056ffb4d395204f994a5c72f90002bcc72179
Robert Kleffner <gamecodeshark@gmail.com>
2021-09-16 16:58:02 +0000

    Basic vm architecture.

--- src/vm.c
+++ src/vm.c
@@ -8,3 +8,28 @@ void initVM(VM * vm) {
 void freeVM(VM * vm) {
 
 }
+
+// Dispatcher function to run the current chunk in the given vm.
+static InterpretResult run(VM * vm) {
+#define READ_BYTE() (*vm.ip++)
+
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction()
+#endif
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_NOP: {
+        return INTERPRET_OK;
+      }
+    }
+  }
+
+#undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk * chunk, VM * vm) {
+    vm->chunk = chunk;
+    vm->ip = chunk->code;
+    return run(vm);
+}
--- src/vm.h
+++ src/vm.h
@@ -5,11 +5,20 @@
 
 typedef struct {
     Chunk * chunk;
+    uint8_t * ip;
 } VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
 
 // Initialize an empty virtual machine state.
 void initVM(VM * vm);
 // Deallocate the given virtual machine state.
 void freeVM(VM * vm);
+// Set the given vm state to run through the given chunk,
+// starting with the first instruction in the chunk.
+InterpretResult interpret(Chunk * chunk, VM * vm);
 
 #endif
