3631c41ecc2831ee0ad9b3e5d38426a81a5b9041
Robert Kleffner <gamecodeshark@gmail.com>
2021-10-09 19:22:27 +0000

    Add a bit more useful stuff to common.

--- src/common.h
+++ src/common.h
@@ -1,6 +1,9 @@
 #ifndef zhenzhu_common_h
 #define zhenzhu_common_h
 
+// Significant portions of this header are taken from the Wren project, including
+// the well-worded comments. See here: https://github.com/wren-lang/wren
+
 // This header contains macros and defines used across the entire Zhenzhu
 // implementation. In particular, it contains "configuration" defines that
 // control how Zhenzhu works. Some of these are only used while hacking on Zhenzhu
@@ -14,10 +17,20 @@
 // include <stdint> unless __STDC_LIMIT_MACROS is defined.
 #define __STDC_LIMIT_MACROS
 #include <stdint.h>
+#include <stdbool.h>
+
+// The Microsoft compiler does not support the "inline" modifier when compiling
+// as plain C.
+#if defined( _MSC_VER ) && !defined(__cplusplus)
+    #define inline _inline
+#endif
 
 // These flags let you control some details of the interpreter's implementation.
 // Usually they trade-off a bit of portability for speed. They default to the
 // most efficient behavior.
+
+// If both pointer tagging and NaN tagging are set to enabled, the build will
+// choose pointer tagging.
 
 // Because Zhenzhu is intended as a target for the statically-typed Boba
 // language, we can make guarantees that runtime type errors will not
@@ -34,10 +47,12 @@
 // scenarios. For example, an application that uses lots of floating
 // point arithmetic could benefit from NaN tagging instead of pointer
 // tagging.
-#ifndef ZHENZHU_POINTER_TAGGING
-    #ifndef ZHENZHU_NAN_TAGGING
-        #define ZHENZHU_NAN_TAGGING 1
-    #endif
+#ifndef ZHENZHU_NAN_TAGGING
+    #define ZHENZHU_NAN_TAGGING 0
+#endif
+
+#if ZHENZHU_POINTER_TAGGING == 1 && ZHENZHU_NAN_TAGGING == 1
+    #error Pointer tagging and NaN tagging cannot both be enabled.
 #endif
 
 // If true, the VM's interpreter loop uses computed gotos. See this for more:
@@ -69,8 +84,66 @@
 // These flags are useful for debugging and hacking on Zhenzhu itself. They are not
 // intended to be used for production code. They default to off.
 
-#ifndef 
+// Run garbage collection before every allocation.
+#define ZHENZHU_DEBUG_GC_STRESS 1
 
-#define DEBUG_TRACE_EXECUTION
+// Log all memory operations.
+#define ZHENZHU_DEBUG_TRACE_MEMORY 1
+
+// Log all garbage collections.
+#define ZHENZHU_DEBUG_TRACE_GC 1
+
+// Log VM state and current instruction before every executed instruction.
+#define ZHENZHU_DEBUG_TRACE_EXECUTION 1
+
+// Assertions are used to validate program invariants. They indicate things the
+// program expects to be true about its internal state during execution. If an
+// assertion fails, there is a bug in Zhenzhu.
+//
+// Assertions add significant overhead, so are only enabled in debug builds.
+#ifdef DEBUG
+
+    #include <stdio.h>
+
+    #define ASSERT(condition, message)                                           \
+        do                                                                       \
+        {                                                                        \
+            if (!(condition))                                                    \
+            {                                                                    \
+                fprintf(stderr, "[%s:%d] Assert failed in %s(): %s\n",           \
+                    __FILE__, __LINE__, __func__, message);                      \
+                abort();                                                         \
+            }                                                                    \
+        } while (false)
+
+    // Indicates that we know execution should never reach this point in the
+    // program. In debug mode, we assert this fact because it's a bug to get here.
+    //
+    // In release mode, we use compiler-specific built in functions to tell the
+    // compiler the code can't be reached. This avoids "missing return" warnings
+    // in some cases and also lets it perform some optimizations by assuming the
+    // code is never reached.
+    #define UNREACHABLE()                                                        \
+        do                                                                       \
+        {                                                                        \
+            fprintf(stderr, "[%s:%d] This code should not be reached in %s()\n", \
+                __FILE__, __LINE__, __func__);                                   \
+            abort();                                                             \
+        } while (false)
+
+#else
+
+    #define ASSERT(condition, message) do { } while (false)
+
+    // Tell the compiler that this part of the code will never be reached.
+    #if defined( _MSC_VER )
+        #define UNREACHABLE() __assume(0)
+    #elif (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))
+        #define UNREACHABLE() __builtin_unreachable()
+    #else
+        #define UNREACHABLE()
+    #endif
+
+#endif
 
 #endif
